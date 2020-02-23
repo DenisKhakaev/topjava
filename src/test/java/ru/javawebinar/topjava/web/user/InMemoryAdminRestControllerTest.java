8297261ca22850e0a207746f15bbfc50e438afab
dkhalaev <denis.khalaev@gmail.com>
2020-02-24 02:30:29 +0300

    task 1

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -23,7 +23,7 @@ public class InMemoryAdminRestControllerTest {
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext("spring-memory-test.xml", "/spring/spring-db.xml", "/spring/spring-app.xml");
         log.info("\n{}\n", Arrays.toString(appCtx.getBeanDefinitionNames()));
         controller = appCtx.getBean(AdminRestController.class);
     }
